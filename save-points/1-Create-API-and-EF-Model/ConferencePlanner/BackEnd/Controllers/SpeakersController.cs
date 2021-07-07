c709dabf39833ac4fa2e1644939bcf3e148b3927
levirap87 <78810449+levirap87@users.noreply.github.com>
2021-07-07 11:37:27 +0530

    Update SpeakersController.cs

--- save-points/1-Create-API-and-EF-Model/ConferencePlanner/BackEnd/Controllers/SpeakersController.cs
+++ save-points/1-Create-API-and-EF-Model/ConferencePlanner/BackEnd/Controllers/SpeakersController.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BackEnd.Models;
-
+//comment
 namespace BackEnd.Controllers
 {
   [Route("api/[controller]")]
